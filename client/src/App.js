94ec045d18683bc9d0f23845c74ea951c54c7622
shershhah <i200523@nu.edu.pk>
2023-12-01 20:05:30 +0500

    App setup routing error handling and router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import "./App.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="*" component={NotFound} />
+        </Switch>
+      </Router>
+    </div>
+  );
 };
+
+function NotFound() {
+  return (
+    <>
+      <h1>404 NOT FOUND!</h1>
+      <center>
+        <p>
+          Page does not exist.
+          <br />
+          Go to{" "}
+          <Link to="/" style={{ color: "black", textDecoration: "underline" }}>
+            Home
+          </Link>
+        </p>
+      </center>
+    </>
+  );
+}
 
 export default App;
